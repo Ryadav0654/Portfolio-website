d3974f5e33a2c29448ac8ddb46b791fc19942ab1
Ravindra Yadav <yadavra2005@gmail.com>
2024-02-22 01:00:34 +0530

    update commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let typed = new Typed('#element', {
+    strings: ['Ravindra Yadav', 'Front-end Developer', 'Coder'],
+    typeSpeed: 120,
+    backSpeed: 60,
+    startDelay: 100,
+    // backDelay: 200,
+    smartBackspace: true,
+    loop: true,
+  });
